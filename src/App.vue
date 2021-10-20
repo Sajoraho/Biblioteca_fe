21d99f2b8734a93290080eabeab02d4bd5ea5e7f
Juan S Arcila <jsab@Juans-Air.fritz.box>
2021-10-21 00:39:58 +0200

    guia_14_vueLogIn and SingUp

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-    <div id="app" class="app">
-        <div class="header">
-            <h1>Biblioteca</h1>
-        </div>
-    </div>
+ <div id="app" class="app">
+   <div class="header">
+     <h1>Biblioteca</h1>
+     <nav>
+       <button v-if="is_auth">Inicio</button>
+       <button v-if="is_auth">Cuenta</button>
+       <button v-if="is_auth">Cerrar Sesión</button>
+       <button v-if="!is_auth" v-on:click="loadLogIn">Iniciar Sesión</button>
+       <button v-if="!is_auth" v-on:click="loadSignUp">Registrarse</button>
+     </nav>
+   </div>
+   <div class="main-component">
+     <router-view
+       v-on:completedLogIn="completedLogIn"
+       v-on:completedSignUp="completedSignUp"
+     >
+     </router-view>
+   </div>
+   <div class="footer">
+     <h2>Biblioteca</h2>
+   </div>
+ </div>
 </template>
 <script>
 export default {
@@ -12,6 +29,32 @@ export default {
     methods: {},
     created: function() {}
 }
+<script>
+export default {
+ name: "App",
+ data: function () {
+   return {
+     is_auth: false,
+   };
+ },
+ components: {},
+ methods: {
+   verifyAuth: function () {
+     if (this.is_auth == false) this.$router.push({ name: "logIn" });
+   },
+   loadLogIn: function () {
+     this.$router.push({ name: "logIn" });
+   },
+   loadSignUp: function () {
+     this.$router.push({ name: "signUp" });
+   },
+   completedLogIn: function (data) {},
+   completedSignUp: function (data) {},
+ },
+ created: function () {
+   this.verifyAuth();
+ },
+};
 </script>
 <style>
 body {
